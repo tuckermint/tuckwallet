70cc474ad87be17f5b1df5a181fe6eaa57681649
tuckermint <info@tuckermint.com>
2020-02-10 23:53:39 -0500

    started initial data load. can't test on localhost due to CORS crap, so we will use cosmos mainnet api node to test

--- dist/bundle.js
+++ dist/bundle.js
@@ -4,15 +4,67 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.test = test;
+exports.loadAccount = loadAccount;
 
 var _sig = require("@tendermint/sig");
 
-function test() {
-  const mnemonic = 'trouble salon husband push melody usage fine ensure blade deal miss twin';
-  const wallet = (0, _sig.createWalletFromMnemonic)(mnemonic); // BIP39 mnemonic string
+const serverAddress = 'https://api.cosmos.network';
+const denom = 'uatom';
 
-  console.log(wallet);
+async function fetchFromServer(path) {
+  let response = await fetch(`${serverAddress}${path}`);
+  let data = await response.json();
+  console.log(data);
+  return data;
+}
+
+function setAccount() {
+  window.document.getElementById('address').value = window.wallet.address;
+}
+
+async function setBalance() {
+  fetchFromServer(`/bank/balances/${window.wallet.address}`).then(data => {
+    const result = data['result'];
+
+    for (const res of result) {
+      if (res['denom'] === denom) {
+        const amount = res['amount'];
+        window.document.getElementById('balance').value = `${amount} ${denom}`;
+        break;
+      }
+    }
+
+    console.log("Did not find correct denom for balance");
+  }).catch(error => {
+    console.log("Error getting balance");
+  });
+}
+
+function setSupply() {
+  fetchFromServer(`/supply/total/${denom}`).then(data => {
+    const supply = data['result'];
+    window.document.getElementById('totalSupply').value = `${supply} ${denom}`;
+  }).catch(error => {
+    console.log("Error getting supply");
+  });
+}
+
+function showMainHeader() {
+  window.document.getElementById('initialHeader').style.display = 'none';
+  window.document.getElementById('header').style.display = '';
+  window.document.getElementById('accountInfo').style.display = '';
+  window.document.getElementById('chainInfo').style.display = '';
+}
+
+function loadAccount() {
+  const mnemonic = window.document.getElementById('mnemonic').value; // BIP39 mnemonic string
+
+  window.wallet = (0, _sig.createWalletFromMnemonic)(mnemonic);
+  console.log(window.wallet);
+  setAccount();
+  setBalance();
+  setSupply();
+  showMainHeader();
 }
 
 },{"@tendermint/sig":4}],2:[function(require,module,exports){
--- index.js
+++ index.js
@@ -1,10 +1,65 @@
 import { createWalletFromMnemonic } from '@tendermint/sig';
 
+const serverAddress = 'https://api.cosmos.network'
+const denom = 'uatom';
 
-export function test(){
-const mnemonic = 'trouble salon husband push melody usage fine ensure blade deal miss twin';
+async function fetchFromServer(path){
+  let response = await fetch(`${serverAddress}${path}`);
+  let data = await response.json()
+  console.log(data);
+  return data;
+}
 
-const wallet = createWalletFromMnemonic(mnemonic); // BIP39 mnemonic string
+function setAccount(){
+    window.document.getElementById('address').value = window.wallet.address;     
+}
 
-console.log(wallet);
+async function setBalance(){
+    fetchFromServer(`/bank/balances/${window.wallet.address}`).then(data => {               
+        const result = data['result'];
+        
+        for(const res of result){
+            if (res['denom'] === denom){                
+                const amount = res['amount'];
+                
+                window.document.getElementById('balance').value = `${amount} ${denom}`;
+                break;
+            }    
+        }        
+        console.log("Did not find correct denom for balance");
+      }).catch(error => {
+        console.log("Error getting balance");
+      });        
+}
+
+function setSupply(){
+    fetchFromServer(`/supply/total/${denom}`)
+      .then(data => {          
+        const supply = data['result'];
+        
+        window.document.getElementById('totalSupply').value = `${supply} ${denom}`;
+        
+      }).catch(error => {
+        console.log("Error getting supply");
+      });       
+}
+
+function showMainHeader(){
+    window.document.getElementById('initialHeader').style.display = 'none';
+    window.document.getElementById('header').style.display = '';
+    window.document.getElementById('accountInfo').style.display = '';
+    window.document.getElementById('chainInfo').style.display = '';
+}
+
+export function loadAccount(){
+    const mnemonic = window.document.getElementById('mnemonic').value; // BIP39 mnemonic string
+
+    window.wallet = createWalletFromMnemonic(mnemonic);    
+
+    console.log(window.wallet);
+    
+    setAccount();
+    setBalance();
+    setSupply();
+    showMainHeader();    
 }
