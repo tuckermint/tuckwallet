3d49b459fe8bd60c49ab0e3ec23e1fd035703b06
tuckermint <info@tuckermint.com>
2020-02-16 20:05:01 -0500

    started delegation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { createWalletFromMnemonic, signTx, verifyTx, createBroadcastTx, BROADCAS
 const serverAddress = 'https://api.cosmos.network'
 const denom = 'uatom';
 const chainId = 'cosmoshub-3';
+const oneHundred = 100.0;
 const oneMillion = 1000000.0;
 
 
@@ -35,6 +36,25 @@ async function getSignMeta(){
         chain_id:       chainId,
         sequence:       data['result']['value']['sequence'],
     }
+}
+
+async function submitTransaction(tx){
+    getSignMeta().then(signMeta => {        
+        const stdTx = signTx(tx, signMeta, window.wallet);
+        console.log('signTx\n\n', JSON.stringify(stdTx, null, 2), '\n');
+        
+        const valid = verifyTx(stdTx, signMeta);
+        console.log('verifyTx\n\n', JSON.stringify(valid, null, 2), '\n');
+        
+        const broadcastTx = createBroadcastTx(stdTx, BROADCAST_MODE_BLOCK);
+        console.log('broadcastTx\n\n', JSON.stringify(broadcastTx, null, 2), '\n');     
+        
+        postToServer("/txs", broadcastTx).then(responseData => {
+            alert(`Transaction result: ${JSON.stringify(responseData)}`);
+        }).catch(error => {
+            alert(`call failed: ${JSON.stringify(error)}`);
+        });
+    });     
 }
 
 function getMicroMacroAmountString(microAmount, microDenom){
@@ -144,6 +164,11 @@ export async function finishSendTuckeratoms(){
         return;
     }
     
+    if (window.wallet.address === receivingAddress){
+        alert("You can't send to yourself!");
+        return;
+    }
+    
     const tuckeratomsToSend = Number(document.getElementById('amountToSend').value);
     const microTuckeratomsToSend = tuckeratomsToSend * oneMillion;
     
@@ -175,64 +200,166 @@ export async function finishSendTuckeratoms(){
         return;
     }
     
-    /*const tx = {
-        fee:  {
-            amount: [{ amount: String(feeInMicroTuckeratoms), denom: denom }],
+    const send = confirm(`Sending ${tuckeratomsToSend} Tuckeratoms to ${receivingAddress}`);
+    
+    if (!send){
+        return;
+    }
+    
+    const tx = {
+        msg: [{
+            type:  'cosmos-sdk/MsgSend',
+            value: {
+                amount: [{
+                    amount: String(microTuckeratomsToSend),
+                    denom: denom
+                }],
+                from_address: window.wallet.address,
+                to_address: receivingAddress
+            }
+        }],
+        fee:  {            
+            amount: [{ denom: denom, amount: String(feeInMicroTuckeratoms) }],
             gas:    String(gas)
         },
         memo: 'TuckWallet',
-        msgs: [{
-            type:  'cosmos-sdk/Send',
-            value: {
-                inputs:  [{
-                    address: window.wallet.address,
-                    coins:   [{ amount: String(microTuckeratomsToSend), denom: denom }]
-                }],
-                outputs: [{
-                    address: receivingAddress,
-                    coins:   [{  amount: String(microTuckeratomsToSend), denom: denom }]
-                }]
-            }
-        }]
-    };*/
-
+    }       
     
-    getSignMeta().then(signMeta => {
-        const tx = {
-            msg: [{
-                type:  'cosmos-sdk/MsgSend',
-                value: {
-                    amount: [{
-                        amount: String(microTuckeratomsToSend),
-                        denom: denom
-                    }],
-                    from_address: window.wallet.address,
-                    to_address: receivingAddress
-                }
-            }],
-            fee:  {            
-                amount: [{ denom: denom, amount: String(feeInMicroTuckeratoms),  }],
-                gas:    String(gas)
-            },
-            memo: 'TuckWallet',
-            //chain_id: chainId,
-            //account_number: signMeta.account_number,
-            //sequence: signMeta.sequence
-        }            
+    submitTransaction(tx);
+}
+// delegate
+
+async function getValidators(){
+    const response = await fetchFromServer('/staking/validators');
+    const validators = response['result'].sort((a, b) => (a['description']['moniker'] > b['description']['moniker']) ? 1 : -1);
+    
+    let options = '';
+    let validatorAddressToNameMap = {};
+    
+    for (const validator of validators){
+        const jailed = validator['jailed'];
         
-        const stdTx = signTx(tx, signMeta, window.wallet);
-        console.log('signTx\n\n', JSON.stringify(stdTx, null, 2), '\n');
+        if (jailed){
+            continue;
+        }        
         
-        const valid = verifyTx(stdTx, signMeta);
-        console.log('verifyTx\n\n', JSON.stringify(valid, null, 2), '\n');
+        const address = validator['operator_address'];
+        const name = validator['description']['moniker'];
+        const commission = String(Number(validator['commission']['commission_rates']['rate']) * oneHundred) + '%';        
         
-        const broadcastTx = createBroadcastTx(stdTx, BROADCAST_MODE_BLOCK);
-        console.log('broadcastTx\n\n', JSON.stringify(broadcastTx, null, 2), '\n');     
+        const microTuckeratoms = validator['tokens'];
+        const tuckeratoms = microTuckeratoms / oneMillion;
         
-        postToServer("/txs", broadcastTx).then(responseData => {
-            alert("call did not crash")
-        }).catch(error => {
-            alert(`call failed: ${error}`)
-        });
-    });
+        validatorAddressToNameMap[address] = name;
+                
+        options += `<option value='${address}'>${name}: ${commission} commission, ${tuckeratoms} Tuckeratoms controlled</option>`;
+    }
+    
+    return [options, validatorAddressToNameMap];
+}
+
+async function getDelegations(validatorAddressToNameMap){
+    const response = await fetchFromServer(`/staking/delegators/${window.wallet.address}/delegations`);
+    const delegations = response['result'].sort((a, b) => (a['shares'] > b['shares']) ? 1 : -1);
+    
+    let options = '';
+    
+    for (const delegation of delegations){
+        const address = delegation['validator_address'];
+        const name = validatorAddressToNameMap[address];        
+        const microTuckeratomBalance = delegation['balance'];
+        const tuckeratomBalance = microTuckeratomBalance / oneMillion;
+        
+        options += `<option value='${address}'>${name}: ${tuckeratomBalance} Tuckeratoms delegated</option>`;
+    }
+    
+    return options;
+}
+
+export function updateMaxDelegationAmount(){
+    const source = window.document.getElementById('source');
+    const text = source.options[source.selectedIndex].text;
+    
+    const amount = window.document.getElementById('amountToDelegate');
+    
+    if (text === 'Undelegated Tuckeratoms'){
+        const currentBalanceInMicroTuckeratoms = window.balances[denom];
+        const currentBalanceInTuckeratoms = currentBalanceInMicroTuckeratoms / oneMillion;
+        amount.max = currentBalanceInTuckeratoms;
+    }
+    else{
+        amount.max = Number(text.split(': ')[1].split(' ')[0]);
+    }
+}
+
+export async function startDelegateTuckeratoms(){
+    const currentBalanceInMicroTuckeratoms = window.balances[denom];
+    const currentBalanceInTuckeratoms = currentBalanceInMicroTuckeratoms / oneMillion;
+    const [validators, validatorAddressToNameMap] = await getValidators();
+    const delegations = await getDelegations(validatorAddressToNameMap);
+    
+    const html = `
+        <h2>Delegate / Redelegate Tuckeratoms</h2>
+        
+        Validator: <select id="validator" style="margin-bottom:20px;">${validators}</select>
+        Source of Tuckeratoms:  <select onchange="tuckwallet.updateMaxDelegationAmount()" id="source" style="width:100%;margin-bottom:20px;"><option value="undelegated">Undelegated Tuckeratoms</option>${delegations}</select>
+        Amount to Delegate (in Tuckeratoms): <input id="amountToDelegate" type="number" style="width:100%;margin-bottom:20px;" min=".000001" max="${currentBalanceInTuckeratoms}" step=".000001">
+        Fee (in Tuckeratoms): <input id="fee" type="number" style="width:100%;margin-bottom:20px;" min="0" step=".000001" value=".005">
+        Gas: <input id="gas" type="number" style="width:100%;margin-bottom:20px;" min="0" step="1" value="50000">
+        
+        <button onclick="tuckwallet.finishDelegateTuckeratoms()">Delegate</button>
+    `;
+    
+    document.getElementById('mainContent').innerHTML = html;
+}
+
+export async function finishDelegateTuckeratoms(){
+    const validatorAddress = document.getElementById('validator').value;
+    const tuckeratomSource = document.getElementById('source').value;
+    
+    const amountToDelegateInTuckeratoms = Number(document.getElementById('amountToDelegate').value);
+    const amountToDelegateInMicroTuckeratoms = amountToDelegateInTuckeratoms * oneMillion;
+    
+    const feeInTuckeratoms = Number(document.getElementById('fee').value);
+    
+    if (feeInTuckeratoms < 0){
+        alert("Fee can't be less than zero!");
+        return;
+    }
+    
+    const feeInMicroTuckeratoms = feeInTuckeratoms * oneMillion;  
+          
+    const gas = Number(document.getElementById('gas').value);    
+    
+    if (gas < 0){
+        alert("Gas can't be less than zero!");
+        return;
+    }
+    
+    const send = confirm(`Delegating ${amountToDelegateInTuckeratoms} Tuckeratoms to ${validatorAddress} from ${tuckeratomSource}`);
+    
+    if (!send){
+        return;
+    }
+    
+    const tx = {
+        msg: [{                
+            type: "cosmos-sdk/MsgDelegate",
+            value: {
+                amount: {
+                    amount: String(amountToDelegateInMicroTuckeratoms),
+                    denom: denom
+                },
+                delegator_address: window.wallet.address,
+                validator_address: validatorAddress
+            }                
+        }],
+        fee: {
+            amount: [ { amount: String(feeInMicroTuckeratoms), denom: denom } ], 
+            gas: String(gas) 
+        },
+        memo: "TuckWallet",
+    }    
+    
+    submitTransaction(tx);
 }
